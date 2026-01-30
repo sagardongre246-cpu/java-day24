988e8bf1724a53ca08714a974d08946e9a1f1648
Sagar Dongare <sagardongre246@gmail.com>
2026-01-30 09:14:56 +0530

    Add files via upload

---
+++ TodoListApp.java
@@ -0,0 +1,64 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class TodoListApp {
+
+    public static void main(String[] args) {
+        ArrayList<String> todoList = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+        int choice;
+
+        do {
+            System.out.println("\n==== TO-DO LIST MENU ====");
+            System.out.println("1. Add Task");
+            System.out.println("2. View Tasks");
+            System.out.println("3. Remove Task");
+            System.out.println("4. Exit");
+            System.out.print("Enter your choice: ");
+            
+            choice = sc.nextInt();
+            sc.nextLine(); // clear buffer
+
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter task: ");
+                    String task = sc.nextLine();
+                    todoList.add(task);
+                    System.out.println("Task added successfully!");
+                    break;
+
+                case 2:
+                    if (todoList.isEmpty()) {
+                        System.out.println("No tasks available.");
+                    } else {
+                        System.out.println("Your Tasks:");
+                        for (int i = 0; i < todoList.size(); i++) {
+                            System.out.println((i + 1) + ". " + todoList.get(i));
+                        }
+                    }
+                    break;
+
+                case 3:
+                    System.out.print("Enter task number to remove: ");
+                    int index = sc.nextInt();
+
+                    if (index > 0 && index <= todoList.size()) {
+                        todoList.remove(index - 1);
+                        System.out.println("Task removed successfully!");
+                    } else {
+                        System.out.println("Invalid task number!");
+                    }
+                    break;
+
+                case 4:
+                    System.out.println("Exiting To-Do List. Goodbye!");
+                    break;
+
+                default:
+                    System.out.println("Invalid choice. Try again!");
+            }
+        } while (choice != 4);
+
+        sc.close();
+    }
+}
